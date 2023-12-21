f9bcd40dc560df39ba74d38101ed70ab28820e2d
kumarsai11 <121474516+kumarsai11@users.noreply.github.com>
2023-12-21 22:22:20 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public static void main(String args[]){
 System.out.println("hello world");
 System.out.println("I am Kumar Sai");
 System.out.println("this is a sample project");
-System.out.println("wipro Technologies");
+System.out.println("wipro Technologies Bangalore");
 }
 }
